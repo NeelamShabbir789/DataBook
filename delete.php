e04a0ad7b1d97394fa1b2761aacec56ed35b9504
NeelamShabbir789 <neelam.dev789@gmail.com>
2019-07-20 13:57:26 +0500

    Final

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$aVar = mysqli_connect('localhost','root','', 'my_db');
-
-$id=$_REQUEST['id'];
-echo $id;
-$query = "DELETE FROM users WHERE id=$id"; 
-$result = mysqli_query($aVar,$query) or die ( mysqli_error());
-header("Location: view.php"); 
-?>
---
+++ login_process.php
@@ -0,0 +1,33 @@
+<?php
+include('code.php');
+
+if(isset($_POST['login']))
+{
+		$email=$_POST['email'];
+		$pas=$_POST['password'];
+		md5($pas);
+
+//create table "admin" and insert "email" and "password" and then apply the below query
+
+	$query="SELECT * from admin WHERE email='$email' and password='$pas'";
+	$exe_query= mysqli_query($db,$query);
+	$found_num_rows= mysqli_num_rows($exe_query);
+	if($found_num_rows==1)
+	{
+	$_SESSION['logedin']=1;
+	$_SESSION['name']='neelam';
+	
+	header('location:index.php');
+	}
+
+	else{
+		echo "Please Register";
+		session_destroy();
+	}
+
+}
+else{
+	header('location:login.php');
+}
+
+?>
